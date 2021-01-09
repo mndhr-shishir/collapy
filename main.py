c437d7797d03d8bdbfe502c9ef124ba1a0efa4b4
mndhr-shishir <sisir.mdr@gmail.com>
2021-01-09 19:11:13 +0545

    add function to multiply 2 numbers

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+def multiply(a, b):
+    return a * b
+
 def add(a, b):
     return a + b
 
 def main():
     res_add = add(3, 4)
+    res_mult = multiply(2, 4)
+
     print(res_add)
+    print(res_mult)
 
 if __name__ == '__main__':
     main()
